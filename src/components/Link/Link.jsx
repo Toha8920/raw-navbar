69cc81e2651913d4899038dbb86aa2c5892eef7d
Toha Islam <islamtoha710@gmail.com>
2023-08-15 20:58:21 +0600

    raw navbar

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const Link = ({ route }) => {
+    const { name, path } = route;
+
+    return (
+        <li className=" mr-12">
+            <a href={path}>{name}</a>
+        </li>
+    );
+};
+
+export default Link;
